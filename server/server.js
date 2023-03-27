4e39d31722ab9c4420a9ee595c071d9c8f22e1fa
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-03-27 14:25:23 +0530

    implement client side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -7,14 +8,16 @@ const postRouter = require('./routes/posts');
 
 connect();
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('this the home page');
+  res.json('this the home page');
 });
 
-app.use('/api/posts', postRouter);
+app.use('/static/api/posts', postRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
